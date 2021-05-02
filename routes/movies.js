eba364cf91efccf0413008e9d81cbd8e4f1c1d6e
Anoop Raj <anooprajsadineni@gmail.com>
2021-05-02 12:33:48 -0500

    Added patch method.

--- routes/movies.js
+++ routes/movies.js
@@ -13,24 +13,36 @@ router.get('/', async(req,res) => {
 
 router.get('/:id', async(req,res) => {
   try {
-    const movies = await Movie.findById(req.params.id)
-    res.json(movies)
+    const movie = await Movie.findById(req.params.id)
+    res.json(movie)
   } catch (err) {
     res.send('Error' + err)
   }
 })
 
 router.post('/', async(req,res) => {
-  const movie = new Movie({
-    title: req.body.title,
-    year: req.body.year,
-    rating: req.body.rating
+  const alien = new Alien({
+    name: req.body.name,
+    tech: req.body.tech,
+    sub: req.body.sub
   })
+
   try {
     const m1 = await movie.save()
     res.json(m1)
   } catch (err) {
-    res.send('Error' + err)
+    res.send('Error')
+  }
+})
+
+router.patch('/:id', async(req,res) => {
+  try {
+    const movie = await Movie.findById(req.params.id)
+    movie.sub = req.body.sub
+    const m1 = await movie.save()
+    res.json(m1)
+  } catch (err) {
+    res.send('Error')
   }
 })
 
