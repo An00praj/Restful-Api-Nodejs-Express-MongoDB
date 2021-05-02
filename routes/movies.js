b172652a2a21d1b9904c6b0457babaf62ef12fa5
Anoop Raj <anooprajsadineni@gmail.com>
2021-05-01 19:21:18 -0500

    Added a route folder to have the movieSchema which includes title, year and rating.

---
+++ routes/movies.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/movie');
+
+router.get('/', async(req,res) => {
+  try {
+    const movies = await Movie.find()
+    res.json(movies)
+  } catch (err) {
+    res.send('Error' + err)
+  }
+})
+
+router.post('/', async(req,res) => {
+  const movie = new Movie({
+    title: req.body.title,
+    year: req.body.year,
+    rating: req.body.rating
+  })
+  try {
+    const m1 = await movie.save()
+    res.json(m1)
+  } catch (err) {
+    res.send('Error' + err)
+  }
+})
+
+module.exports = router;
